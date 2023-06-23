1bcd3bfe25da3a5eb5ce026efb97e94fbcc68313
Kris <kris@kristopherhan.com>
2023-06-23 16:00:48 -0700

    createBooking component

--- frontend/src/components/Bookings/CreateBooking/index.js
+++ frontend/src/components/Bookings/CreateBooking/index.js
@@ -1,6 +1,45 @@
-import    React, { useEffect, useState }            from "react";
-import  { useDispatch }                             from "react-redux";
-import  { useHistory }                              from "react-router-dom";
-import  { createSpotThunk, createSpotImageThunk, getOneSpotThunk }   from "../../../store/spots";
-import                                              "./Bookings.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { createBookingThunk } from "../../../store/bookings";
+import "./Bookings.css";
 
+const CreateBookingForm = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [checkin, setCheckin] = useState('');
+    const [checkout, setCheckout] = useState('');
+    const [guestsNum, setGuestNum] = useState('');
+
+    const [validationErrors, setValidationErrors] = useState('');
+    // validation errors on submit: date booked is free, too many guests,
+    // too far out, user must be logged in, owner of home cant book themselves
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (validationErrors.length > 0) return
+        const bookingFormInputs = {
+            checkin,
+            checkout,
+            guestsNum
+        };
+
+        const newBooking = await dispatch(createBookingThunk(bookingFormInputs));
+        // once booked have a booking confirmation module
+        // return history.push(`/bookings/${newBooking.id}`);
+
+    }
+
+    return (
+        <form
+            className='create-new-booking-form'
+            onSubmit={handleSubmit}
+        >
+
+        </form>
+    )
+}
+
+export default CreateBookingForm;
