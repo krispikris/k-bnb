035cb2005d791cf6129a78855ac34a43911f2088
Kris <kris@kristopherhan.com>
2023-07-07 09:38:44 -0700

    changed the look and functionality of profile button | needs css

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,12 +1,13 @@
 // frontend/src/components/Navigation/ProfileButton.js
-import    React, { useState, useEffect }    from 'react';
-import  { useDispatch }                     from 'react-redux';
-import    * as sessionActions               from '../../store/session';
-import                                           './ProfileButton.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import './ProfileButton.css'
+import { Link } from 'react-router-dom';
 
 function ProfileButton({ user }) {
-  const   dispatch                  = useDispatch();
-  const [ showMenu, setShowMenu ]   = useState(false);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
   const openMenu = () => {
     if (showMenu) return;
@@ -38,16 +39,30 @@ function ProfileButton({ user }) {
       </button>
 
       {showMenu && (
-        <div className="profile-dropdown">
-          <div id='profile-dropdown-1'>Welcome back to Treebnb, {user.firstName}!</div>
-          {/* <div id='profile-dropdown-2'>Welcome back to Treebnb</div> */}
-          <div id='profile-dropdown-2'>{user.username}</div>
-          <div id='profile-dropdown-3'>{user.email}</div>
-          {/* YOOUR SPOTS? */}
-          <div id='profile-dropdown-4'>
+        <ul className="profile-dropdown">
+          <li id='profile-dropdown-1-trips'>
+            <Link to="/trips" className="navigation-button">
+              <button id="nav-button-bold">Trips</button>
+            </Link>
+          </li>
+
+          <li id='profile-dropdown-2-wishlist'>
+            <Link to="/wishlists" className="navigation-button">
+              <button id="nav-button-bold">Wishlists</button>
+            </Link>
+          </li>
+
+          {/* SPOT CREATION: Currently a modal so create form on seperate page */}
+          <li id='profile-dropdown-3-treebnb-your-home'>
+            <Link to="/treebnb-your-home">
+              <button id="nav-button-not-bold">Treebnb your home</button>
+            </Link>
+          </li>
+
+          <li id='profile-dropdown-4-logout'>
             <button id='profile-logout-button' onClick={logout}>Log Out</button>
-          </div>
-        </div>
+          </li>
+        </ul>
       )}
     </>
   );
