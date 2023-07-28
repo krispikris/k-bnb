79bf32f0d38aa91d4d82ee4c3bff8ccd23841a35
Kris <kris@kristopherhan.com>
2023-07-27 23:44:12 -0700

    likes api | completed CREATE & DELETE

--- backend/routes/api/likes.js
+++ backend/routes/api/likes.js
@@ -3,9 +3,42 @@ const router = express.Router();
 const { User, Spot, SpotImage, Booking, Like } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
+// CREATE A LIKE
+router.get('/spotId/:likeId', requireAuth, async (req, res) => {
+    const user = req.user;
+    const { spotId } = req.params;
+    const spot = await Spot.findByPk(spotId);
+
+    if (!user) {
+        return res
+            .status(404)
+            .json({
+                message: "You must be logged in to like a Spot",
+                statusCode: 404
+            })
+    }
+
+    if (!spot) {
+        return res
+            .status(404)
+            .json({
+                message: "Spot couldn't be found",
+                statusCode: 404
+            })
+    }
+
+    const newLike = await Like.create({
+        userId: req.user.id,
+        spotId: spotId
+    });
+
+    return res.json(newLike)
+})
+
 // GET LIKES BY THE CURRENT USER (WISHLIST)
 router.get('/wishlist', requireAuth, async (req, res) => {
-    const wishlist = await Spot.findAll({
+    // const wishlist = await Spot.findAll({            Spot should be attached to Like so the SpotImage can be found
+    const wishlist = await Like.findAll({
         where: { userId: req.user.id }
     })
 
@@ -33,15 +66,15 @@ router.delete('/:likeId', requireAuth, async (req, res) => {
 
     if (!like) {
         res
-        .status(404)
-        .json({
-            message: "Have not liked",
-            statusCode: 404
-        });
+            .status(404)
+            .json({
+                message: "Have not liked",
+                statusCode: 404
+            });
     };
 
     await like.destroy();
-    
+
 })
 
 module.exports = router;
